d17d6a90ea1644afbd5cb2dc1ba6b5cef919b2b1
Junaid <JSidat95@gmail.com>
2021-02-19 15:38:01 +0000

    add factorials file

---
+++ src/factorials/ReverseFactorials.java
@@ -0,0 +1,17 @@
+package factorials;
+
+// import java.util.ArrayList;
+
+public class ReverseFactorials {
+
+	public static void main(String[] args) {
+
+		int j = 120;
+		for (int i = 1; i <= j; i++) {
+			if (j % i == 0) {
+				System.out.println(i);
+
+			}
+		}
+	}
+}
